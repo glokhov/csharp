d498bbbdd3d6264936e6dbce6e6e6d8a0834e3c7
glokhov <105565846+glokhov@users.noreply.github.com>
2025-10-29 00:26:47 +0100

    fdocument project

---
+++ CSharp/src/FDocument/Library.fs
@@ -0,0 +1,107 @@
+﻿namespace FDocument
+
+open System.Xml.Linq
+open FInvoke.Result
+
+module private Option =
+    let inline some (value: 'T) : 'T option =
+        match value with
+        | null -> None
+        | v -> Some v
+
+module XContainer =
+    let inline element (name: XName) (container: XContainer) : XElement option =
+        container.Element(name) |> Option.some
+
+    let inline elements (container: XContainer) : XElement seq =
+        container.Elements()
+
+    let inline elements2 (name: XName) (container: XContainer) : XElement seq =
+        container.Elements(name)
+
+module XElement =
+    let inline element (name: XName) (element: XElement) : XElement option =
+        XContainer.element name element
+
+    let inline elements (element: XElement) : XElement seq =
+        XContainer.elements element
+
+    let inline elements2 (name: XName) (element: XElement) : XElement seq =
+        XContainer.elements2 name element
+
+    let inline firstAttribute (element: XElement) : XAttribute option =
+        element.FirstAttribute |> Option.some
+
+    let inline lastAttribute (element: XElement) : XAttribute option =
+        element.LastAttribute |> Option.some
+
+    let inline attribute (name: XName) (element: XElement) : XAttribute option =
+        element.Attribute(name) |> Option.some
+
+    let inline attributes (element: XElement) : XAttribute seq =
+        element.Attributes()
+
+    let inline attributes2 (name: XName) (element: XElement) : XAttribute seq =
+        element.Attributes(name)
+
+    let inline parse (text: string) : Result<XElement, exn> = invoke XElement.Parse text
+
+    let inline parse2 (text: string) (options: LoadOptions) : Result<XElement, exn> =
+        invoke2 XElement.Parse text options
+
+    let inline load (uri: string) : Result<XElement, exn> =
+        let load: string -> Result<XElement, exn> = invoke XElement.Load
+        load uri
+
+    let inline load2 (uri: string) (options: LoadOptions) : Result<XElement, exn> =
+        let load: string -> LoadOptions -> Result<XElement, exn> = invoke2 XElement.Load
+        load uri options
+
+    let inline save (filename: string) (element: XElement) : Result<unit, exn> =
+        let save: string -> Result<unit, exn> = invoke element.Save
+        save filename
+
+    let inline save2 (filename: string) (options: SaveOptions) (element: XElement) : Result<unit, exn> =
+        let save: string -> SaveOptions -> Result<unit, exn> = invoke2 element.Save
+        save filename options
+
+module XDocument =
+    let inline element (name: XName) (document: XDocument) : XElement option =
+        XContainer.element name document
+
+    let inline elements (document: XDocument) : XElement seq =
+        XContainer.elements document
+
+    let inline elements2 (name: XName) (document: XDocument) : XElement seq =
+        XContainer.elements2 name document
+
+    let inline root (document: XDocument) : XElement option =
+        document.Root |> Option.some
+
+    let inline declaration (document: XDocument) : XDeclaration option =
+        document.Declaration |> Option.some
+
+    let inline documentType (document: XDocument) : XDocumentType option =
+        document.DocumentType |> Option.some
+
+    let inline parse (text: string) : Result<XDocument, exn> =
+        invoke XDocument.Parse text
+
+    let inline parse2 (text: string) (options: LoadOptions) : Result<XDocument, exn> =
+        invoke2 XDocument.Parse text options
+
+    let inline load (uri: string) : Result<XDocument, exn> =
+        let load: string -> Result<XDocument, exn> = invoke XDocument.Load
+        load uri
+
+    let inline load2 (uri: string) (options: LoadOptions) : Result<XDocument, exn> =
+        let load: string -> LoadOptions -> Result<XDocument, exn> = invoke2 XDocument.Load
+        load uri options
+
+    let inline save (filename: string) (document: XDocument) : Result<unit, exn> =
+        let save: string -> Result<unit, exn> = invoke document.Save
+        save filename
+
+    let inline save2 (filename: string) (options: SaveOptions) (document: XDocument) : Result<unit, exn> =
+        let save: string -> SaveOptions -> Result<unit, exn> = invoke2 document.Save
+        save filename options
---
+++ CSharp/test/FDocument.Tests/LibraryTests.fs
@@ -0,0 +1,474 @@
+namespace FDocument.Tests
+
+open System.IO
+open System.Xml.Linq
+open Xunit
+
+module XContainer =
+    open FDocument.XContainer
+
+    let xRoot: XContainer = XElement("root")
+    let xElement: XContainer = XElement("root", XElement("first"), XElement("second"))
+
+    [<Fact>]
+    let ``if an element exists element returns the element (first)`` () =
+        match element (XName.Get("first")) xElement with
+        | Some e -> Assert.Equal("first", e.Name.LocalName)
+        | None -> Assert.Fail()
+
+    [<Fact>]
+    let ``if an element exists element returns the element (second)`` () =
+        match element (XName.Get("second")) xElement with
+        | Some e -> Assert.Equal("second", e.Name.LocalName)
+        | None -> Assert.Fail()
+
+    [<Fact>]
+    let ``if no element exists element returns none`` () =
+        match element (XName.Get("foo")) xElement with
+        | Some _ -> Assert.Fail()
+        | None -> ()
+
+    [<Fact>]
+    let ``if no elements exist element returns none`` () =
+        match element (XName.Get("foo")) xRoot with
+        | Some _ -> Assert.Fail()
+        | None -> ()
+
+    [<Fact>]
+    let ``if elements exist elements returns elements`` () =
+        match elements xElement |> Seq.toList with
+        | a :: _ -> Assert.Equal("first", a.Name.LocalName)
+        | [] -> Assert.Fail()
+
+    [<Fact>]
+    let ``if no elements exist elements returns empty`` () =
+        match elements xRoot |> Seq.toList with
+        | _ :: _ -> Assert.Fail()
+        | [] -> ()
+
+    [<Fact>]
+    let ``if an element exists elements2 returns the element`` () =
+        match elements2 (XName.Get("first")) xElement |> Seq.toList with
+        | a :: _ -> Assert.Equal("first", a.Name.LocalName)
+        | [] -> Assert.Fail()
+
+    [<Fact>]
+    let ``if no element exists elements2 returns empty`` () =
+        match elements2 (XName.Get("foo")) xElement |> Seq.toList with
+        | _ :: _ -> Assert.Fail()
+        | [] -> ()
+
+    [<Fact>]
+    let ``if no elements exist elements2 returns empty`` () =
+        match elements2 (XName.Get("foo")) xRoot |> Seq.toList with
+        | _ :: _ -> Assert.Fail()
+        | [] -> ()
+
+module XElement =
+    open FDocument.XElement
+
+    let xRoot: XElement = XElement("root", XElement("element"))
+    let xFstAttr: XAttribute = XAttribute("first", "First")
+    let xSndAttr: XAttribute = XAttribute("second", "Second")
+    let xElement: XElement = XElement("root", [| xFstAttr; xSndAttr |])
+
+    [<Fact>]
+    let ``if multiple attributes firstAttribute returns first attribute`` () =
+        match firstAttribute xElement with
+        | Some a -> Assert.Equal("first", a.Name.LocalName)
+        | None -> Assert.Fail()
+
+    [<Fact>]
+    let ``if no attributes firstAttribute returns none`` () =
+        match firstAttribute xRoot with
+        | Some _ -> Assert.Fail()
+        | None -> ()
+
+    [<Fact>]
+    let ``if multiple attributes lastAttribute returns last attribute`` () =
+        match lastAttribute xElement with
+        | Some a -> Assert.Equal("second", a.Name.LocalName)
+        | None -> Assert.Fail()
+
+    [<Fact>]
+    let ``if no attributes lastAttribute returns none`` () =
+        match lastAttribute xRoot with
+        | Some _ -> Assert.Fail()
+        | None -> ()
+
+    [<Fact>]
+    let ``if an attribute exists attribute returns the attribute (first)`` () =
+        match attribute (XName.Get("first")) xElement with
+        | Some a -> Assert.Equal("first", a.Name.LocalName)
+        | None -> Assert.Fail()
+
+    [<Fact>]
+    let ``if an attribute exists attribute returns the attribute (second)`` () =
+        match attribute (XName.Get("second")) xElement with
+        | Some a -> Assert.Equal("second", a.Name.LocalName)
+        | None -> Assert.Fail()
+
+    [<Fact>]
+    let ``if no attribute exists attribute returns none`` () =
+        match attribute (XName.Get("foo")) xElement with
+        | Some _ -> Assert.Fail()
+        | None -> ()
+
+    [<Fact>]
+    let ``if no attributes exist attribute returns none`` () =
+        match attribute (XName.Get("foo")) xRoot with
+        | Some _ -> Assert.Fail()
+        | None -> ()
+
+    [<Fact>]
+    let ``if attributes exist attributes returns attributes`` () =
+        match attributes xElement |> Seq.toList with
+        | a :: _ -> Assert.Equal("first", a.Name.LocalName)
+        | [] -> Assert.Fail()
+
+    [<Fact>]
+    let ``if no attributes exist attributes returns empty`` () =
+        match attributes xRoot |> Seq.toList with
+        | _ :: _ -> Assert.Fail()
+        | [] -> ()
+
+    [<Fact>]
+    let ``if an attribute exists attributes2 returns the attribute`` () =
+        match attributes2 (XName.Get("first")) xElement |> Seq.toList with
+        | a :: _ -> Assert.Equal("first", a.Name.LocalName)
+        | [] -> Assert.Fail()
+
+    [<Fact>]
+    let ``if no attribute exists attributes2 returns empty`` () =
+        match attributes2 (XName.Get("foo")) xElement |> Seq.toList with
+        | _ :: _ -> Assert.Fail()
+        | [] -> ()
+
+    [<Fact>]
+    let ``if no attributes exist attributes2 returns empty`` () =
+        match attributes2 (XName.Get("foo")) xRoot |> Seq.toList with
+        | _ :: _ -> Assert.Fail()
+        | [] -> ()
+
+    [<Fact>]
+    let ``if element valid parse returns ok`` () =
+        match parse "<root />" with
+        | Ok e -> Assert.Equal("root", e.Name.LocalName)
+        | Error e -> Assert.Fail(e.Message)
+
+    [<Fact>]
+    let ``if element invalid parse returns error`` () =
+        match parse "" with
+        | Ok _ -> Assert.Fail()
+        | Error e -> Assert.Equal("Root element is missing.", e.Message)
+
+    [<Fact>]
+    let ``if load options none parse2 returns ok`` () =
+        match parse2 "<root> </root>" LoadOptions.None with
+        | Ok e -> Assert.Equal("", e.Value)
+        | Error e -> Assert.Fail(e.Message)
+
+    [<Fact>]
+    let ``if load options whitespaces parse2 returns ok`` () =
+        match parse2 "<root> </root>" LoadOptions.PreserveWhitespace with
+        | Ok e -> Assert.Equal(" ", e.Value)
+        | Error e -> Assert.Fail(e.Message)
+
+    [<Fact>]
+    let ``if element invalid parse2 returns error`` () =
+        match parse2 "" LoadOptions.None with
+        | Ok _ -> Assert.Fail()
+        | Error e -> Assert.Equal("Root element is missing.", e.Message)
+
+    [<Fact>]
+    let ``if element valid load returns ok`` () =
+        let temp = Path.GetTempFileName()
+        File.WriteAllText(temp, "<root />")
+
+        match load temp with
+        | Ok e -> Assert.Equal("root", e.Name.LocalName)
+        | Error e -> Assert.Fail(e.Message)
+
+        File.Delete(temp)
+
+    [<Fact>]
+    let ``if element invalid load returns error`` () =
+        let temp = Path.GetTempFileName()
+        File.WriteAllText(temp, "")
+
+        match load temp with
+        | Ok _ -> Assert.Fail()
+        | Error e -> Assert.Equal("Root element is missing.", e.Message)
+
+        File.Delete(temp)
+
+    [<Fact>]
+    let ``if load options none load2 returns ok`` () =
+        let temp = Path.GetTempFileName()
+        File.WriteAllText(temp, "<root> </root>")
+
+        match load2 temp LoadOptions.None with
+        | Ok e -> Assert.Equal("", e.Value)
+        | Error e -> Assert.Fail(e.Message)
+
+        File.Delete(temp)
+
+    [<Fact>]
+    let ``if load options whitespaces load2 returns ok`` () =
+        let temp = Path.GetTempFileName()
+        File.WriteAllText(temp, "<root> </root>")
+
+        match load2 temp LoadOptions.PreserveWhitespace with
+        | Ok e -> Assert.Equal(" ", e.Value)
+        | Error e -> Assert.Fail(e.Message)
+
+        File.Delete(temp)
+
+    [<Fact>]
+    let ``if element invalid load2 returns error`` () =
+        let temp = Path.GetTempFileName()
+        File.WriteAllText(temp, "")
+
+        match load2 temp LoadOptions.None with
+        | Ok _ -> Assert.Fail()
+        | Error e -> Assert.Equal("Root element is missing.", e.Message)
+
+        File.Delete(temp)
+
+    let formatted = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<root>\n  <element />\n</root>"
+    let unformatted = "<?xml version=\"1.0\" encoding=\"utf-8\"?><root><element /></root>"
+
+    [<Fact>]
+    let ``save successful`` () =
+        let temp = Path.GetTempFileName()
+
+        match xRoot |> save temp with
+        | Ok _ -> Assert.Equal(formatted, File.ReadAllText(temp))
+        | Error e -> Assert.Fail(e.Message)
+
+        File.Delete(temp)
+
+    [<Fact>]
+    let ``save options none save2`` () =
+        let temp = Path.GetTempFileName()
+
+        match xRoot |> save2 temp SaveOptions.None with
+        | Ok _ -> Assert.Equal(formatted, File.ReadAllText(temp))
+        | Error e -> Assert.Fail(e.Message)
+
+        File.Delete(temp)
+
+    [<Fact>]
+    let ``save options formating save2`` () =
+        let temp = Path.GetTempFileName()
+
+        match xRoot |> save2 temp SaveOptions.DisableFormatting with
+        | Ok _ -> Assert.Equal(unformatted, File.ReadAllText(temp))
+        | Error e -> Assert.Fail(e.Message)
+
+        File.Delete(temp)
+
+    [<Fact>]
+    let ``element test`` () =
+        match element (XName.Get("element")) xRoot with
+        | Some e -> Assert.Equal("element", e.Name.LocalName)
+        | None -> Assert.Fail()
+
+    [<Fact>]
+    let ``elements test`` () =
+        match elements xRoot |> Seq.toList with
+        | e :: _ -> Assert.Equal("element", e.Name.LocalName)
+        | [] -> Assert.Fail()
+
+    [<Fact>]
+    let ``elements2 test`` () =
+        match elements2 (XName.Get("element")) xRoot |> Seq.toList with
+        | e :: _ -> Assert.Equal("element", e.Name.LocalName)
+        | [] -> Assert.Fail()
+
+module XDocument =
+    open FDocument.XDocument
+
+    let xDecl: XDeclaration = XDeclaration("1.0", "", "")
+    let xDocType: XDocumentType = XDocumentType("XML", "", "", "")
+    let xElement: XElement = XElement("element")
+    let cElement: obj array = [| xElement |]
+    let xRoot: XElement = XElement("root", cElement)
+    let cRoot: obj array = [| xRoot |]
+    let cDocType: obj array = [| xDocType; xRoot |]
+    let xDoc: XDocument = XDocument()
+    let xDocPlusRoot: XDocument = XDocument(xRoot)
+    let xDocPlusDecl: XDocument = XDocument(declaration = xDecl, content = cRoot)
+    let xDocPlusDocType: XDocument = XDocument(declaration = xDecl, content = cDocType)
+
+    [<Fact>]
+    let ``if root present root returns some`` () =
+        match xDocPlusRoot |> root with
+        | Some e -> Assert.Equal("root", e.Name.LocalName)
+        | None -> Assert.Fail()
+
+    [<Fact>]
+    let ``if no root present root returns none`` () =
+        match xDoc |> root with
+        | Some _ -> Assert.Fail()
+        | None -> ()
+
+    [<Fact>]
+    let ``if declaration present declaration returns some`` () =
+        match xDocPlusDecl |> declaration with
+        | Some d -> Assert.Equal("1.0", d.Version)
+        | None -> Assert.Fail()
+
+    [<Fact>]
+    let ``if no declaration present declaration returns none`` () =
+        match xDocPlusRoot |> declaration with
+        | Some _ -> Assert.Fail()
+        | None -> ()
+
+    [<Fact>]
+    let ``if documentType present documentType returns some`` () =
+        match xDocPlusDocType |> documentType with
+        | Some d -> Assert.Equal("XML", d.Name)
+        | None -> Assert.Fail()
+
+    [<Fact>]
+    let ``if no documentType present documentType returns none`` () =
+        match xDocPlusRoot |> documentType with
+        | Some _ -> Assert.Fail()
+        | None -> ()
+
+    [<Fact>]
+    let ``if document valid parse returns ok`` () =
+        match parse "<root />" with
+        | Ok d -> Assert.Equal("root", d.Root.Name.LocalName)
+        | Error e -> Assert.Fail(e.Message)
+
+    [<Fact>]
+    let ``if document invalid parse returns error`` () =
+        match parse "" with
+        | Ok _ -> Assert.Fail()
+        | Error e -> Assert.Equal("Root element is missing.", e.Message)
+
+    [<Fact>]
+    let ``if load options none parse2 returns ok`` () =
+        match parse2 "<root> </root>" LoadOptions.None with
+        | Ok d -> Assert.Equal("", d.Root.Value)
+        | Error e -> Assert.Fail(e.Message)
+
+    [<Fact>]
+    let ``if load options whitespaces parse2 returns ok`` () =
+        match parse2 "<root> </root>" LoadOptions.PreserveWhitespace with
+        | Ok d -> Assert.Equal(" ", d.Root.Value)
+        | Error e -> Assert.Fail(e.Message)
+
+    [<Fact>]
+    let ``if document invalid parse2 returns error`` () =
+        match parse2 "" LoadOptions.None with
+        | Ok _ -> Assert.Fail()
+        | Error e -> Assert.Equal("Root element is missing.", e.Message)
+
+    [<Fact>]
+    let ``if document valid load returns ok`` () =
+        let temp = Path.GetTempFileName()
+        File.WriteAllText(temp, "<root />")
+
+        match load temp with
+        | Ok d -> Assert.Equal("root", d.Root.Name.LocalName)
+        | Error e -> Assert.Fail(e.Message)
+
+        File.Delete(temp)
+
+    [<Fact>]
+    let ``if document invalid load returns error`` () =
+        let temp = Path.GetTempFileName()
+        File.WriteAllText(temp, "")
+
+        match load temp with
+        | Ok _ -> Assert.Fail()
+        | Error e -> Assert.Equal("Root element is missing.", e.Message)
+
+        File.Delete(temp)
+
+    [<Fact>]
+    let ``if load options none load2 returns ok`` () =
+        let temp = Path.GetTempFileName()
+        File.WriteAllText(temp, "<root> </root>")
+
+        match load2 temp LoadOptions.None with
+        | Ok d -> Assert.Equal("", d.Root.Value)
+        | Error e -> Assert.Fail(e.Message)
+
+        File.Delete(temp)
+
+    [<Fact>]
+    let ``if load options whitespaces load2 returns ok`` () =
+        let temp = Path.GetTempFileName()
+        File.WriteAllText(temp, "<root> </root>")
+
+        match load2 temp LoadOptions.PreserveWhitespace with
+        | Ok d -> Assert.Equal(" ", d.Root.Value)
+        | Error e -> Assert.Fail(e.Message)
+
+        File.Delete(temp)
+
+    [<Fact>]
+    let ``if document invalid load2 returns error`` () =
+        let temp = Path.GetTempFileName()
+        File.WriteAllText(temp, "")
+
+        match load2 temp LoadOptions.None with
+        | Ok _ -> Assert.Fail()
+        | Error e -> Assert.Equal("Root element is missing.", e.Message)
+
+        File.Delete(temp)
+
+    let unformatted = "<?xml version=\"1.0\" encoding=\"utf-8\"?><root><element /></root>"
+    let formatted = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<root>\n  <element />\n</root>"
+
+    [<Fact>]
+    let ``save successful`` () =
+        let temp = Path.GetTempFileName()
+
+        match xDocPlusDecl |> save temp with
+        | Ok _ -> Assert.Equal(formatted, File.ReadAllText(temp))
+        | Error e -> Assert.Fail(e.Message)
+
+        File.Delete(temp)
+
+    [<Fact>]
+    let ``save options none save2`` () =
+        let temp = Path.GetTempFileName()
+
+        match xDocPlusDecl |> save2 temp SaveOptions.None with
+        | Ok _ -> Assert.Equal(formatted, File.ReadAllText(temp))
+        | Error e -> Assert.Fail(e.Message)
+
+        File.Delete(temp)
+
+    [<Fact>]
+    let ``save options formating save2`` () =
+        let temp = Path.GetTempFileName()
+
+        match xDocPlusDecl |> save2 temp SaveOptions.DisableFormatting with
+        | Ok _ -> Assert.Equal(unformatted, File.ReadAllText(temp))
+        | Error e -> Assert.Fail(e.Message)
+
+        File.Delete(temp)
+
+    [<Fact>]
+    let ``element test`` () =
+        match element (XName.Get("root")) xDocPlusRoot with
+        | Some e -> Assert.Equal("root", e.Name.LocalName)
+        | None -> Assert.Fail()
+
+    [<Fact>]
+    let ``elements test`` () =
+        match elements xDocPlusRoot |> Seq.toList with
+        | e :: _ -> Assert.Equal("root", e.Name.LocalName)
+        | [] -> Assert.Fail()
+
+    [<Fact>]
+    let ``elements2 test`` () =
+        match elements2 (XName.Get("root")) xDocPlusRoot |> Seq.toList with
+        | e :: _ -> Assert.Equal("root", e.Name.LocalName)
+        | [] -> Assert.Fail()
